7399d2e8d47e321a185501d5145df64ea2010914
Jewel <mustaphaamidatoyindamola@gmail.com>
2020-12-06 03:30:28 +0100

    first nodeJs

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+
+const app = express();
+// keep static files (e.g css,image files) inside a folder
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true})) ;
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/app.html") ;
+});
+
+app.post( "/", (req, res) => {
+    var firstName = req.body.fName ;
+    var lastName = req.body.lName ;
+    var email = req.body.eMail ;
+    console.log(firstName, lastName, email);
+}) ;
+
+app.listen( process.env.PORT || 3000, function () {
+    console.log("server is running on port 3000") ;
+});
+
+// enter "nodemon app.js" in the terminal to resume the project
